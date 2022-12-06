c774fc56f739fe474a6b948748f781f82e1958d1
AnikBudhathoki <anik.budhathoki@sjsu.edu>
2022-12-05 19:38:21 -0800

    Final

--- src/studentMain.java
+++ src/studentMain.java
@@ -6,6 +6,7 @@ import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.Stack;
 
+//Main program
 public class studentMain {
     public static LinkedList<assignStudent> ENGRschool = new LinkedList<assignStudent>();
     public static ArrayList<assignStudent> SCIschool = new ArrayList<assignStudent>();
